811b1541526098d3d91d7da56322afce276d52a3
yohannesata <yohannesataklti9@gmail.com>
2024-04-24 20:08:46 +0300

    Revert "update 2"
    This reverts commit c3156c4ed4f2bca587a1daebe57a4b49559d04d9.

--- wp-content/plugins/elementor/modules/ai/module.php
+++ wp-content/plugins/elementor/modules/ai/module.php
@@ -297,9 +297,8 @@ class Module extends BaseModule {
 		if ( ! $app->is_connected() ) {
 			throw new \Exception( 'not_connected' );
 		}
-		$request_ids = $this->get_request_ids( $data['payload'] );
 
-		$result = $app->get_image_prompt_enhanced( $data['prompt'], [], $request_ids );
+		$result = $app->get_image_prompt_enhanced( $data['prompt'] );
 		$this->throw_on_error( $result );
 
 		return [
